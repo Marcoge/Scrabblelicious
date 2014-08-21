59212da11cdc0eeba41f970baf049a4cd5dd34f9
Marco <m.gester@gmx.net>
2014-08-21 22:44:04 +0200

    Rudimentary UI

---
+++ Scrabblelicious/AppContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabblelicious
+{
+    public sealed class AppContext
+    {
+        private string _availableletters;
+        
+        public AppContext()
+        {
+            _availableletters = "labernicht";
+            Cells = new ObservableCollection<Cell>();
+            var temp = createCells();
+            foreach (Cell c in temp)
+            {
+                Cells.Add(c);
+            }
+
+        }
+
+        public String AvailableLetters { get { return _availableletters; } set { } }
+        public ObservableCollection<Cell> Cells { get; set; }
+
+        private IEnumerable<Cell> createCells()
+        {
+            List<Cell> temp = new List<Cell>();
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+			    {
+			        temp.Add(new Cell(i, j));
+			    }
+            }
+            return temp;
+        }
+
+    }
+}
---
+++ Scrabblelicious/Cell.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabblelicious
+{
+    public class Cell: INotifyPropertyChanged
+    {
+        Char _letter;
+        Pos _position;
+        bool _verticalBlock;
+        bool _horizontalBlock;
+
+        public Cell(int x, int y)
+        {
+            _position = new Pos(x, y);
+        }
+
+        public Char Letter
+        {
+            get
+            {
+                return _letter;
+            }
+
+            set
+            {
+                if (value != _letter)
+                {
+                    _letter = value;
+                    OnPropertyChanged("Letter");
+                }
+            }
+        }
+        public Pos Position { get; set; }
+        public int X { get { return _position.X; } }
+        public int Y { get { return _position.Y; } }
+        public bool VerticalBlock { get { return _verticalBlock; } set { value = _verticalBlock; } }
+        public bool HorizontalBlock { get { return _horizontalBlock; } set { value = _horizontalBlock; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(String propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
